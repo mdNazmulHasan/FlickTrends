f0d7cb63539f9a18fc069200f1e6202e1dfe011a
mdNazmulHasan <najmul.hasan10@gmail.com>
2025-01-29 15:15:56 +0600

    enhance Card component with like button and add PropTypes validation; update styles for better layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-const Card = ({ title,  }) => {
-    return (
-        <div className="card">
-            <h2>{title}</h2>
-        </div>
-    )
-}
+const Card = ({ title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  // useEffect(() => {
+  //   return () => {
+  //     effect
+  //   };
+  // }, [input])
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <button onClick={() => {
+        setHasLiked(!hasLiked);
+      }}>{hasLiked?'❤️':"🤍"}</button>
+    </div>
+  );
+};
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 const App = () => {
-    return (
-        <>
-         <h2>Functional arrow component</h2>
-         <Card title="minion"/>
-         <Card title="id invaded"/>
-        </>
-   )
-}
+  
+  return (
+    <div className="card-container">
+      <Card title="minion" />
+      <Card title="id invaded" />
+    </div>
+  );
+};
 
-export default App
+export default App;
