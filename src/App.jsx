ac0e2b360b46b0b86d71aa88e2599a6871b7a3f2
mdNazmulHasan <najmul.hasan10@gmail.com>
2025-01-29 22:18:56 +0600

    refactor App component structure, integrate Tailwind CSS, and remove unused assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 
-const Card = ({ title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  // useEffect(() => {
-  //   return () => {
-  //     effect
-  //   };
-  // }, [input])
+function App() {
   return (
-    <div className="card">
-      <h2>{title}</h2>
-      <button onClick={() => {
-        setHasLiked(!hasLiked);
-      }}>{hasLiked?'❤️':"🤍"}</button>
-    </div>
+    <main>
+      <div className="pattern"/>
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">movies</span> you'll enjoy without hassle</h1></header>
+      </div>
+    </main>
   );
-};
-Card.propTypes = {
-  title: PropTypes.string.isRequired,
-};
-
-const App = () => {
-  
-  return (
-    <div className="card-container">
-      <Card title="minion" />
-      <Card title="id invaded" />
-    </div>
-  );
-};
+}
 
 export default App;
