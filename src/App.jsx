b8d85fbe1d5c267b48e95d3e665a7ff5151055d7
mdNazmulHasan <najmul.hasan10@gmail.com>
2025-01-31 12:53:44 +0600

    add Appwrite integration for search count tracking; enhance MovieCard with additional movie details and implement debounced search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useDebounce } from "react-use";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -17,28 +19,40 @@ const API_OPTIONS = {
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const fetchMovies = async () => {
+
+  useDebounce(
+    () => {
+      setDebouncedSearchTerm(searchTerm);
+    },
+    500,
+    [searchTerm]
+  );
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const url = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const url = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(url, API_OPTIONS);
       const data = await response.json();
       setMovieList(data.results || []);
-      console.log(data);
+      if (query && data.results[0]) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       setErrorMessage("Error fetching movies, please try again later");
-      console.log("🚀 ~ fetchMovies ~ error:", error);
     } finally {
       setIsLoading(false);
     }
   };
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
   return (
     <main>
       <div className="pattern" />
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 
-const MovieCard = ({ movie: { poster_path, title, overview } }) => {
+const MovieCard = ({
+  movie: { poster_path, title, vote_average, release_date, original_language },
+}) => {
   return (
-    <li>
-      <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
-      <h3>{title}</h3>
-      <p>{overview}</p>
-    </li>
+    <div className="movie-card">
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500${poster_path}`
+            : "/no-movie.png"
+        }
+        alt={title}
+      />
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="content">
+          <div className="rating">
+            <img src="/star.svg" alt="star icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "n/a"}</p>
+            <span>•</span>
+            <p className="lang">{original_language}</p>
+            <span>•</span>
+            <p className="year">
+              {release_date ? release_date.split("-")[0] : "n/a"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
